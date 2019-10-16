9dd822935128a4789b4b1a5d12b55b0edb8b3591
Nattapon Rakthong <nattapon.rakthong@gmail.com>
2019-10-16 10:50:11 +0700

    Use typescript

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-exports.printMsg = function() {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+
+function printMsg() {
     console.log("This is a message from the demo package");
 }
+exports.printMsg = printMsg;
